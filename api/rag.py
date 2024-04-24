8805ee2c701759d8bf62f891fad99a0966982e17
pavelp <pavelp.fullstack@gmail.com>
2024-04-24 11:01:34 +0300

    reviwer notes applied, api extension for query

--- api/rag.py
+++ api/rag.py
@@ -1,3 +1,9 @@
+import os
+import re
+import numpy as np
+from typing import List
+from sklearn.metrics.pairwise import cosine_similarity
+
 # neo4j:
 from langchain_community.graphs import Neo4jGraph
 from langchain_community.vectorstores import Neo4jVector
@@ -10,138 +16,141 @@ from langchain_core.output_parsers import StrOutputParser
 
 # retrieval:
 from langchain.chains import RetrievalQAWithSourcesChain
-from langchain.memory import ConversationBufferMemory
 
 from config import Config
 
 
 class RAGModel:
 
+    @staticmethod
+    def init_prompts(prompt_dir: str):
+        with open(os.path.join(prompt_dir, "query_template_0.txt"), 'r', encoding='utf-8') as f:
+            qt0 = f.read()
+
+        with open(os.path.join(prompt_dir, "query_template_1.txt"), 'r', encoding='utf-8') as f:
+            qt1 = f.read()
+
+        with open(os.path.join(prompt_dir, "query_template_2.txt"), 'r', encoding='utf-8') as f:
+            qt2 = f.read()
+
+        with open(os.path.join(prompt_dir, "query_template_3.txt"), 'r', encoding='utf-8') as f:
+            qt3 = f.read()
+
+        return qt0, qt1, qt2, qt3
+
     def __init__(self, cfg: Config) -> None:
         self.config = cfg
 
-        # llm initialization
-        self.llm = GigaChat(
-            credentials=self.config.LLM_AUTH,
-            temperature=0.3,
-            n=1,
-            model="GigaChat-Plus",  # 32k context window
-            repetition_penalty=1.0,
-            verify_ssl_certs=False
-        )
+        self.embeddings = GigaChatEmbeddings(credentials=self.config.LLM_AUTH, verify_ssl_certs=False)
+        self.vector_store = Neo4jVector.from_existing_index(self.embeddings,
+                                                            url=self.config.NEO4J_URI,
+                                                            username=self.config.NEO4J_USERNAME,
+                                                            password=self.config.NEO4J_PASSWORD,
+                                                            node_label="Bullet",
+                                                            index_name="bullet",
+                                                            keyword_index_name="word",
+                                                            search_type='hybrid',
+                                                            )
 
-        # parser initialization
-        self.parser = StrOutputParser()
-
-        # embeddings initialization
-        self.embeddings = GigaChatEmbeddings(
-            credentials= self.config.LLM_AUTH,
-            verify_ssl_certs=False
-        )
-
-        # promt template
-        self.template = """
-                    Задача: Анализировать заданный запрос и предоставлять детализированные ответы, опираясь на документы, правила и требования.
-
-                    Инструкции:
-
-                    1. При ответе на вопросы, особенно те, которые связаны с правилами или нормативными документами, активно ссылайтесь на номера пунктов, статей и разделов документов.
-                    2. Ваши ответы должны быть максимально точными и содержать не только ссылки на документы, но и объяснения их применения к данному запросу.
-
-                    Вопрос: {question}
-                    Контекст: {context}
-                    
-                    Ответ: Предоставьте ваш ответ, опираясь на указанные выше указания, отформатируйте текст под выдачу в телеграмм
-                    """
-
-        # prompt initialization
-        self.prompt = ChatPromptTemplate.from_template(self.template)
-
-        neo4j_vector = Neo4jVector.from_existing_index(
-            self.embeddings,
+        self.graph = Neo4jGraph(
             url=self.config.NEO4J_URI,
             username=self.config.NEO4J_USERNAME,
             password=self.config.NEO4J_PASSWORD,
-            index_name="vector"
-            # search_type = 'hybrid'
-        )
+            database=self.config.NEO4J_DATABASE)
 
-        # llm retrival chain
-        self.chain = RetrievalQAWithSourcesChain.from_chain_type(
-            self.llm,
-            chain_type="stuff",  # "stuff", "map_rerank", "refine"
-            retriever=neo4j_vector.as_retriever(search_kwargs={"k": 5}),
-            return_source_documents=False,
-            reduce_k_below_max_tokens=False,
-            max_tokens_limit=32000,
-            chain_type_kwargs={
-                "verbose": False,
-                "prompt": self.prompt,  # step_back_prompt
-                "document_variable_name": "context",
-                "memory": ConversationBufferMemory(
-                    memory_key='history',
-                    input_key='question'
-                ),
-            }
-        )
+        self.llm = GigaChat(model="GigaChat-Pro",  # "GigaChat-Pro" / "GigaChat-Plus" (32k)
+                            temperature=0.3,
+                            top_p=0.2,
+                            n=1,
+                            repetition_penalty=1,
+                            credentials=self.config.LLM_AUTH,
+                            verify_ssl_certs=False)
 
-    # sub query functions
-    def step_back_prompt(self, text: str):
+        self.parser = StrOutputParser()
+        self.qt0, self.qt1, self.qt2, self.qt3 = RAGModel.init_prompts(self.config.prompts_dir)
+        self.prompt = ChatPromptTemplate.from_template(self.qt0)
+        self.llm.get_num_tokens(self.qt0)
+        self.retriever = self.vector_store.as_retriever(
+            search_kwargs={
+                "k": 10, 'score_threshold': 0.95, 'lambda_mult': 0.25
+            })
+
+        self.chain = RetrievalQAWithSourcesChain.from_chain_type(self.llm,  # pro / plus
+                                                                 chain_type="stuff",
+                                                                 # "stuff" / "map_rerank" / "refine"
+                                                                 retriever=self.retriever,
+                                                                 return_source_documents=True,
+                                                                 reduce_k_below_max_tokens=True,
+                                                                 max_tokens_limit=8000,  # up to 32k with plus model
+                                                                 chain_type_kwargs={"verbose": False,
+                                                                                    "prompt": self.prompt,
+                                                                                    "document_variable_name": "context"})
+
+    def extract_question(self, text: str, template_id: int = 1):
         chain = self.llm | self.parser
 
-        template = f"""
-                    Задача: Анализировать представленные данные и вывести из них высокоуровневые концепции и основные принципы.
+        template = ''
+        if template_id == 1:
+            template = self.qt1.format(text=text)
+        elif template_id == 2:
+            template = self.qt2.format(text=text)
+        elif template_id == 3:
+            template = self.qt3.format(text=text)
+        else:
+            raise ValueError("Invalid template_id value. Expected a number in a range 1 .. 3")
 
-                    Инструкции:
-                    Прежде чем приступить к абстракции, важно:
-                    1. Определить и выделить ключевые детали и специфику представленного материала.
-                    2. Анализировать связи между деталями для понимания более глубоких закономерностей и взаимосвязей.
-                    3. Структурировать полученные данные, выделяя общие элементы и паттерны.
-                    4. Формулировать высокоуровневые концепции и принципы, опираясь на проанализированные данные.
+        question = chain.invoke(template).replace('/', ' ')
+        return question
 
-                    Текст: \"{text}\"
+    def handle_query(self, query: str, template_id: int = 1) -> any:
+        inquiry = str(query.replace('\n', ' '))
+        question = self.extract_question(inquiry, template_id)
+        resp = self.chain.invoke({"question": question}, return_only_outputs=True)
+        if 'question' not in resp:
+            resp['question'] = question
+        return resp
 
-                    Проанализируйте и сформулируйте общие концепции и основные принципы на основе представленных деталей в виде развернутых вопросов.
-                    """
+    def find_relevant_bullets(self, inquiry: str) -> List[str]:
+        match = re.search(r'(\d+\.\d+)(?:\.\d+)?', inquiry)
 
-        model_response = chain.invoke(template)
-        return model_response
+        if match:
+            number = match.group(1)
 
-    def extract_question(self, text: str):
-        chain = self.llm | self.parser
+            cypher = f"""
+                        MATCH (n:Document)
+                        WHERE n.bullet STARTS WITH '{number}' AND n.bullet IS NOT NULL
+                        RETURN n.text
+                        """
+            mentioned_bullets = self.graph.query(cypher)
 
-        template = f"""
-                    Мне нужно твоё содействие в анализе следующего делового письма.
-                    Извлеки из него всю важную информацию для системы RAG. 
-                    Нужны вопросы, ключевые параметры, и основные темы обсуждения. Вот текст письма:
+            if mentioned_bullets:
+                texts = [bullet['n.text'] for bullet in mentioned_bullets]
 
-                    Текст: \"{text}\"
+                all_embeddings = [self.embeddings.embed_query(text) for text in texts] + [
+                    self.embeddings.embed_query(inquiry)]
+                inquiry_embedding = all_embeddings[-1]
+                bullet_embeddings = all_embeddings[:-1]
 
-                    Разбери письмо на следующие компоненты и предоставь в виде шаблона:
+                similarities = cosine_similarity([inquiry_embedding], bullet_embeddings)[0]
+                top_indices = np.argsort(similarities)[::-1][:3]
+                top_texts = [texts[i] for i in top_indices]
 
-                    '1. Все поставленные вопросы.
-                    2. Перечень ключевых параметров, упомянутых в тексте.
-                    3. Основные темы, которые обсуждаются.'
-                    
-                    Используй структурированный подход, чтобы я мог легко использовать эти данные для запросов в системе RAG.
-                    Без дополнительных комментариев.
-                    """
+                return top_texts
+            else:
+                return ["Отсутствуют"]
+        else:
+            return ["Отсутствуют"]
 
-        model_response = chain.invoke(template)
-        return model_response
+    @staticmethod
+    def get_bullets_or_source(resp: any):
+        bullets = []
+        for doc in resp['source_documents']:
+            bullet = doc.metadata.get('bullet')
+            if bullet is None:
+                bullet = doc.metadata.get('source')
+            bullets.append(bullet)
+        return bullets
 
-    # final function
-    def get_response(self, request: str) -> str:
-        inquiry = str(request.replace('\n', ' '))
-        # abstraction = step_back_prompt(llm, parser, inquiry)
-        # print("\n\n---\n\n", abstraction)
-        question = self.extract_question(inquiry)
-        # print("\n\n---\n\n", question, "\n\n---\n\n")
-        response = self.chain.invoke(
-            {"question": question},
-            return_only_outputs=True
-        )["answer"]
-        return response
 
 # for testing
 if __name__ == "__main__":
@@ -150,6 +159,6 @@ if __name__ == "__main__":
 
     rag = RAGModel(config)
     while True:
-        query = input()
-        response = rag.get_response(query)
+        test_query = input()
+        response = rag.handle_query(test_query)
         print(response)
